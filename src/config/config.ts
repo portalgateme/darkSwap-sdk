f233d099396251cae179a4f83ddfc79c74ef864d
Peter0x2 <peter0x2@outlook.com>
2025-12-19 15:57:10 +0800

    0.1.39, wait for more confirmtion for allowance

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,6 +5,17 @@ export const legacyTokenConfig: { [chainId: number]: string[] } = {
   [ChainId.HARDHAT]: ['0xdac17f958d2ee523a2206206994597c13d831ec7']
 };
 
+const confirmationsConfig: { [chainId: number]: number } = {
+  [ChainId.MAINNET]: 3,
+  [ChainId.ARBITRUM_ONE]: 3,
+  [ChainId.BASE]: 3,
+  [ChainId.SEPOLIA]: 3,
+  [ChainId.HARDHAT]: 3,
+}
+
+const DEFAULT_CONFIRMATIONS = 3;
+
+export const getConfirmations = (chainId: number) => confirmationsConfig[chainId] || DEFAULT_CONFIRMATIONS;
 
 export const DEFAULT_FEE_RATIO = 300n;
 
